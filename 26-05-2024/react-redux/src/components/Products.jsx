57199f9e9882f2b1b8aea49fd4ed11d99a3b4071
Mukul <msoni7718@gmail.com>
2024-06-02 10:42:09 +0530

    React- Redux completed

--- 26-05-2024/react-redux/src/components/Products.jsx
+++ 26-05-2024/react-redux/src/components/Products.jsx
@@ -1,23 +1,50 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import { addProducts } from '../redux/product/ProductReducer';
+import { addProducts, fetchProducts, fetchProductsById } from '../redux/product/ProductReducer';
 
 function Products() {
 
     const dispatch = useDispatch();
-    const products = useSelector(store => store.product.products)
+    const { loading, products, productData, errorMessage } = useSelector(store => store.product)
+
+    const handleFetchProductByID = () => {
+        dispatch(fetchProductsById(1));
+    }
     useEffect(() => {
         if (products.length === 0) {
-            fetch('https://dummyjson.com/products')
-                .then(res => res.json())
-                .then(data => dispatch(addProducts(data.products)));
+            //Api calling  with the component, we will be going into api caling by redux thunk for asynchronous calls.
+            // fetch('https://dummyjson.com/products')
+            //     .then(res => res.json())
+            //     .then(data => dispatch(addProducts(data.products)));
+
+
+            dispatch(fetchProducts());
         }
     }, [products])
+
+    // if (loading) {
+    //     return (
+    //         <div>
+    //             <h1>Loading...</h1>
+    //         </div>
+    //     )
+    // }
 
     return (
         <div>
             <h1>Product</h1>
-            <p>Total Products : {products.length}</p>
+            {loading && <h1>Loading...</h1>}
+
+            {
+                errorMessage && <p>{errorMessage}</p>
+            }
+            {
+                !loading && !errorMessage && products.length > 0 &&
+                <p> Total Products : {products.length}</p>
+            }
+
+            <button onClick={handleFetchProductByID}> Fetch Product By Id</button>
+            <h3>Title: {productData.title}</h3>
         </div>
     )
 }
--- 26-05-2024/react-redux/src/redux/product/ProductReducer.js
+++ 26-05-2024/react-redux/src/redux/product/ProductReducer.js
@@ -1,15 +1,59 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice, rejectWithValue } from "@reduxjs/toolkit";
+import { createAsyncThunk } from "@reduxjs/toolkit"
 export const productSlice = createSlice({
     name: "product",
     initialState: {
-        products: []
+        loading: false,
+        products: [],
+        productData: {},
+        errorMessage: ""
     },
     reducers: {
-        addProducts: (state, action) => {
-            state.products = action.payload
-        }
+        // addProducts: (state, action) => {
+        //     state.products = action.payload
+        // }
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchProducts.pending, (state) => {
+            state.loading = true;
+        })
+        builder.addCase(fetchProducts.fulfilled, (state, action) => {
+            state.loading = false;
+            state.products = action.payload;
+        });
+        builder.addCase(fetchProducts.rejected, (state, action) => {
+            state.loading = false;
+            console.log(action);
+            state.errorMessage = action.payload;
+
+        })
+        builder.addCase(fetchProductsById.pending, (state) => {
+            state.loading = true;
+        });
+        builder.addCase(fetchProductsById.fulfilled, (state, action) => {
+            state.loading = false;
+            state.productData = action.payload
+        })
     }
+})
+
+export const fetchProducts = createAsyncThunk('products/fetching',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await fetch('https://dummyjson.com/products');
+            const data = await response.json();
+            return data.products;
+        } catch (error) {
+            console.log(error);
+            return rejectWithValue("Error=> Operation failed !! Please try again!!")
+        }
+    })
+
+
+export const fetchProductsById = createAsyncThunk('products/fetchById', async (prodId) => {
+    const response = await fetch(`https://dummyjson.com/products/${prodId}`);
+    const data = await response.json();
+    return data;
 })
 
 export const { addProducts } = productSlice.actions;
